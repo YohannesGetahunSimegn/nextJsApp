acead1908cb87f231f3311b61f79ba2651155dc4
Yohannes G.Simegne <ygetahun37@gmail.com>
2024-11-01 15:34:22 +0300

    Meta data

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,12 +4,17 @@ import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import { Suspense } from "react";
 import CardWrapper from "@/app/ui/dashboard/cards";
+import { Metadata } from "next";
 
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
   CardSkeleton,
 } from "@/app/ui/skeletons";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 export default async function Page() {
   return (
